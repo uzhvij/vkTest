a8471c4a60564569bd9b44567f9c2937a090ee4a
Alexander Bondarenko <uzhvij@gmail.com>
2020-12-25 20:16:15 +0200

    adding vk sdk

--- app/src/main/java/com/blogspot/uzhvij/vktest/MainActivity.java
+++ app/src/main/java/com/blogspot/uzhvij/vktest/MainActivity.java
@@ -3,12 +3,28 @@ package com.blogspot.uzhvij.vktest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.vk.api.sdk.utils.VKUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "myLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.text1);
+
+        Log.d(TAG, "create");
+
+        String[] fingerprints = VKUtils.getCertificateFingerprint(this, this.getPackageName());
+        Log.d(TAG, fingerprints[0] + " " + fingerprints.length);
+
+        textView.setText("wow");
+
     }
 }
